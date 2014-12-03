33baa7b91c2b8fe699bb9c391555d60cd91917fe
Thunder07 <m.a.hot.m+git@gmail.com>
2014-12-03 20:59:31 +0000

    Update Addon.sh

--- Addon.sh
+++ Addon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-yum install wget
+yum install wget -y
 wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 rpm -ivh mysql-community-release-el7-5.noarch.rpm
-yum install mysql-server
+yum install mysql-server -y
 
 yum install ajenti-v ajenti-v-nginx ajenti-v-mysql ajenti-v-php-fpm php-mysql -y
 systemctl restart ajenti
