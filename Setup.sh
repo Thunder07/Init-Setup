e12be88376a102310a769d09c48e8ecfd7e2be70
Thunder07 <m.a.hot.m+git@gmail.com>
2014-12-03 21:26:13 +0000

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -16,9 +16,14 @@ rpm -ivh https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-2.noarc
 echo ':: Adding Ajenti repo'
 rpm -ivh http://repo.ajenti.org/ajenti-repo-1.0-1.noarch.rpm
 
+rpm -ivh http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+yum install mysql-server -y
+
 echo ':: Installing package'
 yum install ajenti -y
-yum install ajenti-v ajenti-v-nginx ajenti-v-mysql ajenti-v-php-fpm php-mysql -y
+yum install ajenti-v ajenti-v-nginx ajenti-v-mysql ajenti-v-php-fpm php-mysql ajenti-v-ftp-pureftpd -y
+firewall-cmd --permanent --zone=public --add-port=80/tcp
+firewall-cmd --permanent --zone=public --add-service=http
 
 echo ':: Done! Open https://<address>:8000 in browser'
 
@@ -28,6 +33,7 @@ firewall-cmd --reload
 systemctl restart ajenti
 
 
+yum install wget -y
 
 
 
