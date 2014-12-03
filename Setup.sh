ee266f48da1703079678217c98b02f7c16ec3536
Thunder07 <m.a.hot.m+git@gmail.com>
2014-12-03 20:46:29 +0000

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 read -p "Enter IP Address For Whitelisting: " IP
 
-sudo yum install denyhosts -y
-systemctl restart denyhosts
+#sudo yum install denyhosts -y
+#echo "sshd: $IP">>/etc/hosts.allow
+#systemctl restart denyhosts
 
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
@@ -17,6 +18,7 @@ rpm -ivh http://repo.ajenti.org/ajenti-repo-1.0-1.noarch.rpm
 
 echo ':: Installing package'
 yum install ajenti -y
+yum install ajenti-v ajenti-v-nginx ajenti-v-mysql ajenti-v-php-fpm php-mysql -y
 
 echo ':: Done! Open https://<address>:8000 in browser'
 
@@ -24,6 +26,8 @@ firewall-cmd --permanent --zone=public --add-port=8000/tcp
 firewall-cmd --reload
 
 systemctl restart ajenti
+
+
 
 
 
