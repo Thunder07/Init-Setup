34d149be29873e81b2a2491dd2cd4543bb6cc9e5
Thunder07 <m.a.hot.m+git@gmail.com>
2014-12-03 19:57:35 +0000

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo ':: Adding EPEL repo'
+rpm -ivh http://download.fedoraproject.org/pub/epel/beta/7/x86_64/epel-release-7-1.noarch.rpm || true
+
+echo ':: Adding Ajenti repo'
+rpm -ivh http://repo.ajenti.org/ajenti-repo-1.0-1.noarch.rpm
+
+echo ':: Installing package'
+yum install ajenti -y
+
+echo ':: Done! Open https://<address>:8000 in browser'
+
+firewall-cmd --permanent --zone=public --add-port=8000/tcp
+firewall-cmd --reload
+
+systemctl restart ajenti
