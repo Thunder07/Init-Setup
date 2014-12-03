ded90994e17a5b2ada0baf231b710f6225173d17
Thunder07 <m.a.hot.m+git@gmail.com>
2014-12-03 23:04:40 +0000

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -22,14 +22,26 @@ yum install mysql-server -y
 echo ':: Installing package'
 yum install ajenti -y
 yum install ajenti-v ajenti-v-nginx ajenti-v-mysql ajenti-v-php-fpm php-mysql ajenti-v-ftp-pureftpd -y
+
+systemctl restart pure-ftpd
+sleep 5
+
+echo "PassivePortRange           40110 40510" >> /etc/pure-ftpd/pure-ftpd.conf
+echo "TLS           2" >> /etc/pure-ftpd/pure-ftpd.conf
+echo "TLSCipherSuite           HIGH:MEDIUM:+TLSv1:!SSLv2:+SSLv3" >> /etc/pure-ftpd/pure-ftpd.conf
+
+firewall-cmd --permanent --zone=public --add-port=21/tcp
+firewall-cmd --permanent --zone=public --add-service=ftp
+firewall-cmd --permanent --zone=public --add-port=40110-40510/tcp
 firewall-cmd --permanent --zone=public --add-port=80/tcp
 firewall-cmd --permanent --zone=public --add-service=http
-
-echo ':: Done! Open https://<address>:8000 in browser'
-
 firewall-cmd --permanent --zone=public --add-port=8000/tcp
 firewall-cmd --reload
 
+echo ':: Done! Open https://<address>:8000 in browser'
+
+
+systemctl restart pure-ftpd
 systemctl restart ajenti
 
 
