69bf4af7f6855e320e4bd13e0e30f4d3249f501f
Thunder07 <m.a.hot.m+git@gmail.com>
2014-12-03 22:57:48 +0000

    Update Addon.sh

--- Addon.sh
+++ Addon.sh
@@ -4,4 +4,9 @@
 
 yum install ajenti-v-ftp-pureftpd -y
 
+echo "PassivePortRange           40110 40510" >> /etc/pure-ftpd/pure-ftpd.conf
+echo "TLS           2" >> /etc/pure-ftpd/pure-ftpd.conf
+echo "TLSCipherSuite           HIGH:MEDIUM:+TLSv1:!SSLv2:+SSLv3" >> /etc/pure-ftpd/pure-ftpd.conf
+firewall-cmd --permanent --zone=public --add-port=40110-40510/tcp
+
 systemctl restart ajenti
