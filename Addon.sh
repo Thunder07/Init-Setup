1bc42cd0bf82c6f8af1c2820670dd768cdef18d2
Thunder07 <m.a.hot.m+git@gmail.com>
2014-12-04 13:40:15 +0000

    Update Addon.sh

--- Addon.sh
+++ Addon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-
+rpm -Uvh --oldpackage http://apt.sw.be/source/denyhosts-2.6-5.el7.rf.src.rpm
 rpm -Uvh --oldpackage http://mirror.metrocast.net/fedora/epel/6/i386/epel-release-6-8.noarch.rpm
 
