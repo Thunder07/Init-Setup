8060e4d0ed043f5b5079d41ef4c60ef6b09fcd31
Thunder07 <m.a.hot.m+git@gmail.com>
2014-12-04 15:00:12 +0000

    Update Addon.sh

--- Addon.sh
+++ Addon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rpm -Uvh http://mirror.karneval.cz/pub/linux/fedora/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
-
+rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
 
